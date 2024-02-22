545b3e548705d29c97ee967c304cab952061af7a
Nikitaa-Patel <159388484+Nikitaa-Patel@users.noreply.github.com>
2024-02-22 17:50:37 +0000

    Add files via upload

--- final_assignment2.py
+++ final_assignment2.py
@@ -40,8 +40,7 @@ app.layout = html.Div([
         html.Label("Select Statistics:"),
         dcc.Dropdown(
             id='dropdown-statistics',
-            options=[{'label':'Yearly Statistics','value':'Yearly Statistics'},
-            {'label':'Recession Period Statistics','value':'Recession Period Statistics'}],
+            options= dropdown_options,
             placeholder='Select a report type',
             value='Select Statistics'
         )
@@ -49,7 +48,8 @@ app.layout = html.Div([
     html.Div(dcc.Dropdown(
             id='select-year',
             options=[{'label': i, 'value': i} for i in year_list],
-            placeholder='select-year'
+            placeholder='select a year',
+            value='select-year'
         )),
     html.Div([#TASK 2.3: Add a division for output display
     html.Div(id='output-container', className='chart-grid', style={'display':'flex'}),])
@@ -86,16 +86,16 @@ def update_output_container(selected_statistics,input_year):
         yearly_rec=recession_data.groupby('Year')['Automobile_Sales'].mean().reset_index()
         R_chart1 = dcc.Graph(
             figure=px.line(yearly_rec, 
-                x='years',
-                y='sales',
+                x='Year',
+                y='Automobile_Sales',
                 title="Average Automobile Sales fluctuation over Recession Period"))
 
 #Plot 2 Calculate the average number of vehicles sold by vehicle type       
         # use groupby to create relevant data for plotting
         average_sales = recession_data.groupby('Vehicle_Type')['Automobile_Sales'].mean().reset_index()                           
         R_chart2  = dcc.Graph(figure=px.bar(average_sales,
-        x='Vehicle Types',
-        y='sales',
+        x='Vehicle_Type',
+        y='Automobile_Sales',
         title="Average vehicles sold by vehicle type"))
         
 # Plot 3 Pie chart for total expenditure share by vehicle type during recessions
@@ -112,7 +112,7 @@ def update_output_container(selected_statistics,input_year):
         unemp_data=recession_data.groupby(['Vehicle_Type','unemployment_rate'])['Automobile_Sales'].mean().reset_index()
         R_chart4=dcc.Graph(
             figure=px.bar(unemp_data,
-            x='unemployment rate',y='automobile sales',
+            x='unemployment_rate',y='Automobile_Sales',
             color='Vehicle_Type',
             labels={'unemployment_rate':'Unemployment Rate','Automobile_Sales':'Average Automobile Sales'},
             title="Effect of Unemployment rate on vehicle type"
@@ -135,12 +135,14 @@ def update_output_container(selected_statistics,input_year):
 #plot 1 Yearly Automobile sales using line chart for the whole period.
         yas= data.groupby('Year')['Automobile_Sales'].mean().reset_index()
         Y_chart1 = dcc.Graph(figure=px.line(yas,
-        x='year',y='sales',
+        x='Year',y='Automobile_Sales',
         title="yearly sales for whole period"))
             
 # Plot 2 Total Monthly Automobile sales using line chart.
+        monthly_sales= data.groupby('Month')['Automobile_Sales'].sum().reset_index()
+               
         Y_chart2 = dcc.Graph(figure=px.line(monthly_sales,
-        x='month',y='sales',
+        x='Month',y='Automobile_Sales',
         title="total monthly sales in a year{}".format(input_year)
         ))
 
@@ -148,7 +150,7 @@ def update_output_container(selected_statistics,input_year):
         avr_vdata=yearly_data.groupby('Vehicle_Type')['Automobile_Sales'].mean().reset_index()
 
         Y_chart3 = dcc.Graph( figure=px.bar(avr_vdata,
-        x='vehicle type',y='sales',
+        x='Vehicle_Type',y='Automobile_Sales',
         title='Average Vehicles Sold by Vehicle Type in the year {}'.format(input_year)
         ))
 
@@ -162,8 +164,8 @@ def update_output_container(selected_statistics,input_year):
 
 #TASK 2.6: Returning the graphs for displaying Yearly data
         return [
-                html.Div(className='chart-item', children=[html.Div(children=Y_chart1),html.Div(children=Y_chart2)],style={'display':'flex'}),
-                html.Div(className='chart-item', children=[html.Div(children=Y_chart3),html.Div(children=Y_chart4)],style={'display':'flex'})
+                html.Div(className='chart-item', children=[html.Div(children=Y_chart1),html.Div(children=Y_chart2)],style={'display':'flex','flex-direction': 'column'}),
+                html.Div(className='chart-item', children=[html.Div(children=Y_chart3),html.Div(children=Y_chart4)],style={'display':'flex','flex-direction': 'column'})
                 ]
         
     else:
